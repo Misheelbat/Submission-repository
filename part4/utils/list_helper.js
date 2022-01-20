81fe5f542d6526f241b4ddacacf414d37f6449f5
Misheelbat <p_misheel@me.com>
2022-01-20 18:32:58 +0100

    done 4.5 - 4.7

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash/array');
+
 const dummy = (blogs) => {
   if (Array.isArray(blogs)) {
     return 1;
@@ -19,7 +21,56 @@ const totalLikes = (blogs) => {
   return total;
 };
 
+const favoriteBlog = (blogs) => {
+  const numberOfLikes = blogs.map((blog) => blog.likes);
+  const mostLikes = Math.max(...numberOfLikes);
+  const index = numberOfLikes.indexOf(mostLikes);
+  return blogs[index];
+};
+
+const mostBlog = (blogs) => {
+  const author = blogs.map((b) => b.author);
+  const all = _.uniq(author);
+
+  const every = [];
+  for (i in all) {
+    const single = blogs.filter((b) => b.author === all[i]);
+    every.push(single);
+  }
+  let larg = every.reduce(
+    (max, el, i, arr) => (el.length > arr[max].length ? i : max),
+    0
+  );
+
+  const mostLikedAuthor = {
+    author: all[larg],
+    blogs: every[larg].length,
+  };
+  return mostLikedAuthor;
+};
+const mostLikes = (blogs) => {
+  const author = blogs.map((b) => b.author);
+  const all = _.uniq(author);
+  const every = [];
+  for (i in all) {
+    const single = blogs.filter((b) => b.author === all[i]);
+    every.push(single);
+  }
+  let arrayOfLikes = every.map((e) =>
+    e.map((l) => l.likes).reduce((t, a) => (t += a), 0)
+  );
+  const mostLikes = Math.max(...arrayOfLikes);
+  const likedAuthor = {
+    author: all[arrayOfLikes.indexOf(mostLikes)],
+    likes: mostLikes,
+  };
+  return likedAuthor;
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
+  mostBlog,
+  mostLikes,
 };
