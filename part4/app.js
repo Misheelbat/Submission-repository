8eeb7afc5ffadf135af66773f2a555a93b330eab
Misheelbat <p_misheel@me.com>
2022-01-21 20:15:16 +0100

    added users

--- part4/app.js
+++ part4/app.js
@@ -5,6 +5,7 @@ const app = express();
 require('express-async-errors');
 const cors = require('cors');
 const blogRouter = require('./controllers/blogs');
+const userRouter = require('./controllers/users');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
@@ -25,6 +26,7 @@ mongoose
 app.use(cors());
 app.use(express.json());
 app.use('/api/blogs', blogRouter);
+app.use('/api/users', userRouter);
 app.use(middleware.errorHandler);
 
 module.exports = app;
