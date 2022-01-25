ecf001161cf79fd822eb9a8f1db30b14c04883cb
Misheelbat <p_misheel@me.com>
2022-01-25 17:37:57 +0100

    added login function

--- part5/src/App.js
+++ part5/src/App.js
@@ -1,24 +1,59 @@
-import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import React, { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
+import loginService from './services/login';
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
-
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
-
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const user = await loginService.login({ username, password });
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (error) {
+      console.log('credentials', error.message);
+    }
+  };
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="text"
+          value={password}
+          name="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      {user === null ? loginForm() : <div>{user.name} logged in</div>}
+      {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
