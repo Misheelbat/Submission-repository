82f4dea6a6fcb3afc3e4e813a0c1375cede57987
Misheelbat <p_misheel@me.com>
2022-01-28 20:47:28 +0100

    done 5.3-5.7

---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { newAnec } from '../reducers/anecdoteReducer';
+import { useDispatch } from 'react-redux';
+
+export default function AnecdoteForm() {
+  const dispatch = useDispatch();
+  const add = e => {
+    e.preventDefault();
+    const content = e.target.anecdote.value;
+    dispatch(newAnec(content));
+    e.target.anecdote.value = '';
+  };
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={add}>
+        <input name="anecdote" />
+        <button type="submit">create</button>
+      </form>
+    </div>
+  );
+}
---
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -0,0 +1,45 @@
+const anecdotesAtStart = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+];
+
+const getId = () => (100000 * Math.random()).toFixed(0);
+
+const asObject = anecdote => {
+  return {
+    content: anecdote,
+    id: getId(),
+    votes: 0,
+  };
+};
+
+const initialState = anecdotesAtStart.map(asObject);
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'VOTE':
+      console.log(action.id);
+      const [vote] = state.filter(an => an.id === action.id);
+      const newVote = { ...vote, votes: vote.votes + 1 };
+      const newState = state.map(state =>
+        state.id !== action.id ? state : newVote
+      );
+      return newState;
+    case 'NEW':
+      const newAnectode = action.data;
+      return state.concat(newAnectode);
+    default:
+      return state;
+  }
+};
+export const newAnec = anec => {
+  return {
+    type: 'NEW',
+    data: { content: anec, id: getId(), votes: 0 },
+  };
+};
+export default reducer;
